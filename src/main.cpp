701a16e3003b5a2ee15eedc2b084a52d719015e1
pupu08429-svg <pupu08429@gmail.com>
2026-05-18 16:27:35 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,47 @@
 // Copyright 2022 NNTU-CS
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+  const int kN = 60;
+  std::srand(std::time(nullptr));
 
-  while (count--)
-    train.addCar(false);
+  // Все выключены
+  Train train_off;
+  int count = kN;
+  while (count--) {
+    train_off.addCar(false);
+  }
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+  std::cout << " Все выключены " << std::endl;
+  std::cout << "Длина: " << train_off.getLength() << std::endl;
+  std::cout << "Операций: " << train_off.getOpCount() << std::endl;
+
+  // Все включены
+  Train train_on;
+  count = kN;
+  while (count--) {
+    train_on.addCar(true);
+  }
+
+  std::cout << "\n Все включены " << std::endl;
+  std::cout << "Длина: " << train_on.getLength() << std::endl;
+  std::cout << "Операций: " << train_on.getOpCount() << std::endl;
+
+  // Случайное распределение
+  Train train_rand;
+  count = kN;
+  while (count--) {
+    bool light = std::rand() % 2;
+    train_rand.addCar(light);
+  }
+
+  std::cout << "\n Случайное распределение " << std::endl;
+  std::cout << "Длина: " << train_rand.getLength() << std::endl;
+  std::cout << "Операций: " << train_rand.getOpCount() << std::endl;
+
   return 0;
 }
